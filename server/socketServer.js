86501169cc75d96716ace841c67b4b38aaf91eb4
6Soup <kys12103@gmail.com>
2025-08-22 10:37:22 +0900

    Revert "주석 선택 기능 추가"
    This reverts commit a897151add1461aa1479988aec3cf81c48c17be0.

--- server/socketServer.js
+++ server/socketServer.js
@@ -108,13 +108,6 @@ io.on("connection", (socket) => {
         }
     });
 
-    socket.on('peer-select',({ roomId, tool }) => {
-        const room = rooms[roomId];
-        if (room && room.hostId) {
-            io.to(room.hostId).emit('tool-select', { tool });
-        }
-    });
-
     const forwardToHost = (eventName) => {
         socket.on(eventName, ({ roomId, ...rest }) => {
             const room = rooms[roomId];
